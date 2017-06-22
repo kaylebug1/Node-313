531b44dab34e6d86d65e8a8a1ec60c5c382f7bdd
Kayle Sillito <sil14003@byui.edu>
2017-06-22 10:54:14 -0600

    added mail price

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+//var calcPrice = require('./calculate.js');
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -9,12 +11,62 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
+app.get('/getRate', findPrice);
+
+function findPrice(request, response) {
+	var weight = request.query.ounce;
+	var type = request.query.type;
+	
+	var info = {
+		weight: weight,
+		type: type
+	};
+
+	var cost = calculate(info);
+	if(cost!=0){
+	response.render("pages/price.ejs", { cost : cost, weight: weight, type:type} );
+	}
+	else{
+	response.render("pages/fail.ejs", { weight: weight, type:type} );		
+	}
+
+};
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
 
+function calculate(info){
+	var cost = 0.00;
+	var weight = info["weight"];
+	var type = info["type"];
+	weight--;
+	if(type == "stamped letter"){
+		if(weight < 3){
+			cost = .49 +(.21* weight);
+		}
+	}
+	if(type == "metered letter"){
+		if(weight < 3){
+			cost = .46 +(.21* weight);
+		}
+	}
+	if(type == "large envelope"){
+		if(weight < 13){
+			cost = .98 +(.21* weight);
+		}
+	}
+	if(type == "parcel"){
+		var weightTemp = weight-3;
+		if(weight < 4){
+			cost = 2.67;
+		}
+		else if(weight < 13){
+			cost = 2.67 +(.18 * weightTemp);
+		}
+	}
+
+	return cost.toFixed(2);
+
+}
